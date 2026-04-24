88df8ec56464e57ddf162f7902835e753e4eb2e7
西村和眞（NISHIMURAKazuma <NISHIMURAKazuma@CST0100091>
2026-04-24 16:59:53 +0900

    7-11

--- Java100_questions_cac2nd/src/lesson07/challenge11/CastleTown.java
+++ Java100_questions_cac2nd/src/lesson07/challenge11/CastleTown.java
@@ -23,37 +23,73 @@
 
 package lesson07.challenge11;
 
-
 //ここにSamuraiクラスを記述
+class Samurai {
 
+	protected String name;
+
+	void fight() {
+		System.out.println("戦うよ～。");
+	}
+
+	void work() {
+		System.out.println("何かして働くよ～");
+	}
+
+}
 
 //ここにRetainerクラスを記述
+class Retainer extends Samurai {
 
+	/**
+	 * 
+	 */
+	public Retainer() {
+
+	}
+
+}
 
 //ここにRoninクラスを記述
+class Ronin extends Samurai {
 
+	public Ronin() {
+	}
+
+}
 
 public class CastleTown {
 
-    public static void main(String[] args) {
-        System.out.println("5人の侍を配列に詰めます。\n");
+	public static void main(String[] args) {
+		System.out.println("5人の侍を配列に詰めます。\n");
 
+		//ここに適切な処理を記述
+		Samurai[] samurais = new Samurai[5];
+		for (int i = 0; i < samurais.length; i++) {
+			int num = (int) (Math.random() * 10) % 2;
+			if (num == 0) {
+				samurais[i] = new Retainer();
+			} else if (num == 1) {
+				samurais[i] = new Ronin();
+			}
+			System.out.println("詰め終わりました。\n");
+			System.out.println("それぞれの人数を表示してみます。\n");
 
-        //ここに適切な処理を記述
+			int retainerCount = 0;
+			int roninCount = 0;
 
+			//ここに適切な処理を記述
+			for (int j = 0; j < samurais.length; j++) {
+				if (samurais[j] instanceof Retainer) {
+					retainerCount++;
+				} else {
+					roninCount++;
+				}
 
-        System.out.println("詰め終わりました。\n");
-        System.out.println("それぞれの人数を表示してみます。\n");
+			}
 
-
-        int retainerCount = 0;
-        int roninCount = 0;
-
-
-        //ここに適切な処理を記述
-
-
-        System.out.println("藩士：" + retainerCount + "人");
-        System.out.println("\n浪人：" + roninCount + "人");
-    }
+			System.out.println("藩士：" + retainerCount + "人");
+			System.out.println("\n浪人：" + roninCount + "人");
+		}
+	}
 }
